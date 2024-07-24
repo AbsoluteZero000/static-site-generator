7069959e172388207537a3c15aa4ca8dfe84d6b2
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-07-24 10:30:56 +0300

    added TextNode Class

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode
+def main():
+    textnode = TextNode("This is a text node", "bold", "https://github.com/Absolutezero000")
+    print(textnode)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type 
+        self.url = url
+
+    def __eq__(self, target):
+        return self.text == target.text and self.text_type == target.text_type and self.url == target.url
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
