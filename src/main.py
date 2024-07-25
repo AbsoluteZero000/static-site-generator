e1e0b623d76ec478968272c8517d5e3ea0af6df7
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-07-25 08:27:10 +0300

    added text_node to HTMLnode function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,23 @@
 from textnode import TextNode
+from htmlnode import HTMLNode 
+
+def text_node_to_html_node(text_node):
+    match(text_node.text_type):
+        case "text":
+            return LeafNode(value=text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
+        case _:
+            raise ValueError("This is not a valid type of text_node")
+
 def main():
     textnode = TextNode("This is a text node", "bold", "https://github.com/Absolutezero000")
     print(textnode)
